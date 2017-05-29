aefd683db02c0a510a09091dedd293c59403e8de
Ben Greenman <benjaminlgreenman@gmail.com>
2017-05-29 13:37:49 -0400

    ** fix deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "require-typed-check")
 (define deps '("base" "typed-racket-lib" "typed-racket-more"))
-(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib" "math-doc" "math-lib" "typed-racket-doc"))
 (define pkg-desc "require/typed without contracts")
 (define version "0.2")
 (define pkg-authors '(ben))
